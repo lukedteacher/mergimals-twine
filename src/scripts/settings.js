cf3ad45a60fcbfed8062e28a94fbde9756ce9427
lukedteacher <lukedteacher@gmail.com>
2025-04-02 19:09:22 -0500

    various refactorings

---
+++ src/scripts/settings.js
@@ -0,0 +1,10 @@
+Setting.addList('version', {
+    label   : 'choose a game version',
+    list    : ['fusion', 'fission']
+});
+
+Setting.addList('difficulty', {
+	label   : 'choose a difficulty level.',
+	list    : ['easy', 'normal', 'hard'],
+	default : 'normal'
+});
