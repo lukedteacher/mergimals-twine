05434734631a5bf4aa5c24610bc7b8991cbfc882
lukedteacher <lukedteacher@gmail.com>
2025-08-15 10:54:47 -0500

    added some character sheets and tweaked zahara encounter

--- src/scripts/gain.js
+++ src/scripts/gain.js
@@ -17,20 +17,9 @@
     const $gainBox = $(document.createElement('div'))
       .addClass('gain-notification-box');
     
-    const $gainIcon = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg'))
-      .addClass('gain-icon')
-      .attr('viewBox', '0 0 100 100')
-      .appendTo($gainBox);
-    
-    const $gainIconPath = $(document.createElementNS('http://www.w3.org/2000/svg', 'path'))
-      .attr('stroke-width', '2')
-      .attr('stroke', 'white')
-      .attr('d', 'M22 15V16L14 18.5L7 16.6V18H1V7H9L15.2 9.3C16.2 9.7 17 10.8 17 12H19C20.7 12 22 13.3 22 15M5 16V9H3V16H5M19.9 14.6C19.7 14.2 19.4 14 19 14H13.6C13.1 14 12.5 13.9 12 13.8L9.7 13L10.3 11.1L12.7 11.9C13 11.9 15 12 15 12C15 11.6 14.8 11.3 14.4 11.2L8.6 9H7V14.5L14 16.4L19.9 14.6Z')
-      .appendTo($gainIcon);
-    
     const $gainSpan = $(document.createElement('span'))
       .addClass('gain-verb-span')
-      .text(' GAIN: ')
+      .text('🫴 GAIN: ')
       .appendTo($gainBox);
     
     const $notificationSpan = $(document.createElement('span'))
